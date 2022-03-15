bc189d1c9107f8b935508befc85ccfe47fc03e0e
Ryan Toronto <ryanto@gmail.com>
2022-03-14 23:37:53 -0400

    fix vercel envs with vite config

--- packages/next-deploy-notifications/vite.config.ts
+++ packages/next-deploy-notifications/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import typescript from "@rollup/plugin-typescript";
 import { readFileSync } from "fs";
-import { resolve, join } from "path";
-import { readdir, writeFile, readFile } from "fs/promises";
+import { resolve } from "path";
 
 const pkg = JSON.parse(
   readFileSync(new URL("./package.json", import.meta.url), { encoding: "utf8" })
@@ -12,37 +11,41 @@ const pkg = JSON.parse(
 let externalDependencies = ["dependencies", "peerDependencies"].reduce(
   (result, group) => [
     ...result,
-    ...(pkg[group] ? Object.keys(pkg[group]) : [])
+    ...(pkg[group] ? Object.keys(pkg[group]) : []),
   ],
   []
 );
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.VERCEL": "process.env.VERCEL",
+    "process.env.VERCEL_GIT_COMMIT_SHA": "process.env.VERCEL_GIT_COMMIT_SHA",
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/react.tsx"),
-      name: "next-deploy-notifications"
+      name: "next-deploy-notifications",
     },
     rollupOptions: {
       external: [...externalDependencies, /react\/.+/, /^node:/],
       input: {
         react: resolve(__dirname, "src/react.tsx"),
-        api: resolve(__dirname, "src/api.ts")
+        api: resolve(__dirname, "src/api.ts"),
       },
       plugins: [typescript(), singleTypeDef()],
       output: [
         {
           entryFileNames: "[name].js",
-          format: "es"
+          format: "es",
         },
         {
           entryFileNames: "[name].[format].js",
-          format: "cjs"
-        }
-      ]
-    }
-  }
+          format: "cjs",
+        },
+      ],
+    },
+  },
 });
 
 function singleTypeDef() {
@@ -51,14 +54,14 @@ function singleTypeDef() {
 
     async generateBundle(options, bundle) {
       let files = Object.keys(bundle);
-      let defFiles = files.filter(file => file.match(/^\w+\.d\.ts$/));
-      let types = defFiles.map(file => bundle[file].source);
+      let defFiles = files.filter((file) => file.match(/^\w+\.d\.ts$/));
+      let types = defFiles.map((file) => bundle[file].source);
       let content = types.join("\n");
       this.emitFile({
         type: "asset",
         fileName: "types.d.ts",
-        source: content
+        source: content,
       });
-    }
+    },
   };
 }
