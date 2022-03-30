2e077dbf16f9498503bfb61c0e8cebc28e4c9db8
Ryan Toronto <ryanto@gmail.com>
2022-03-30 13:46:35 -0400

    Add debug option, make checking a bit more consistent

--- packages/next-deploy-notifications/src/hooks/use-has-new-deploy.ts
+++ packages/next-deploy-notifications/src/hooks/use-has-new-deploy.ts
@@ -18,6 +18,7 @@ let getCurrentVersion = async (endpoint: string) => {
 type HookOptions = {
   interval?: number;
   endpoint?: string;
+  debug?: boolean;
 };
 
 type HookValues = {
@@ -28,34 +29,57 @@ type HookValues = {
 type UseHasNewDeploy = (options?: HookOptions) => HookValues;
 
 let useHasNewDeploy: UseHasNewDeploy = (options = {}) => {
+  let debug = (message: string) => {
+    if (options.debug) {
+      console.log(...["[Deploy notifications] ", message]);
+    }
+  };
+
   let [hasNewDeploy, setHasNewDeploy] = useState<boolean>(false);
   let [currentVersion, setCurrentVersion] = useState<string>("unknown");
+  let [lastFetched, setLastFetched] = useState<number>();
 
   let windowFocused = useWindowFocus();
   let interval = options.interval ?? 30_000;
   let endpoint = options.endpoint ?? "/api/has-new-deploy";
   let isUnknown = currentVersion === "unknown";
-  let pollingInterval = !hasNewDeploy && windowFocused ? interval : null;
+
+  let loopInterval = interval < 3_000 ? interval : 3_000;
+  let loopOrNotInterval = !hasNewDeploy && windowFocused ? loopInterval : null;
 
   useInterval(async () => {
-    let { version } = await getCurrentVersion(endpoint);
+    debug("Looping...");
 
-    if (!isUnknown && currentVersion !== version) {
-      setHasNewDeploy(true);
+    let enoughTimeHasPassed =
+      !lastFetched || Date.now() >= lastFetched + interval;
+
+    if (enoughTimeHasPassed && !isUnknown) {
+      debug("Fetching version");
+      let { version } = await getCurrentVersion(endpoint);
+      debug(`Version ${version}`);
+
+      if (currentVersion !== version) {
+        debug("Found new deploy");
+        setHasNewDeploy(true);
+      }
+
+      setCurrentVersion(version);
+      setLastFetched(Date.now());
     }
-
-    setCurrentVersion(version);
-  }, pollingInterval);
+  }, loopOrNotInterval);
 
   useEffect(() => {
     let fetchInitialVersion = async () => {
+      debug("Fetching initial version");
       let { version } = await getCurrentVersion(endpoint);
       if (version === "unknown") {
         console.warn(
           "[next-deploy-notifications] Could not find current app version."
         );
       } else {
+        debug(`Version ${version}`);
         setCurrentVersion(version);
+        setLastFetched(Date.now());
       }
     };
 
